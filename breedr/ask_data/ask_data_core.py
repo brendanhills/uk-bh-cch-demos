b0bff8a50dcc97cb0ac765e7183fc0dc40900e4f
Brendan Hills <brendanhills@google.com>
2025-02-08 17:58:58 +0000

    Updated to use genai and Gemini 2.0

--- breedr/ask_data/ask_data_core.py
+++ breedr/ask_data/ask_data_core.py
@@ -1,8 +1,8 @@
-import vertexai
-from vertexai.generative_models import GenerativeModel, Part, SafetySetting
+from google import genai
+from google.genai import types
+from google.genai.types import Part, SafetySetting
 
-DEFAULT_MODEL_NAME = "gemini-1.5-flash-001"
-DEFAULT_SAFETY_SETTING = SafetySetting.HarmBlockThreshold.BLOCK_ONLY_HIGH
+DEFAULT_MODEL_NAME = "gemini-2.0-flash-001"
 
 import logging
 logger = logging.getLogger()
@@ -10,57 +10,70 @@ logger = logging.getLogger()
 
 class AskData:
 
-  def __init__(self, project_id, region, model_name=DEFAULT_MODEL_NAME, safety_setting=DEFAULT_SAFETY_SETTING):
+  client = None
+
+  def __init__(self, project_id, region, model_name=DEFAULT_MODEL_NAME):
     self.model_name = model_name
-    self.safety_setting = safety_setting
     self.project_id = project_id
     self.region = region
-    self.initialize_ai(self.project_id, self.region, self.model_name, self.safety_setting)
+    self.initialize_ai(self.project_id, self.region, self.model_name)
 
 
   def convert_instructions_to_yaml(self,text: str) -> str:
 
-    generation_config = {
-      "max_output_tokens": 8192,
-      "temperature": 1,
-      "top_p": 0.95,
-    }
-
-    insructions_part = Part.from_text(text)
-    prompt = """convert this text to structured YAML"""
-    response = self.model.generate_content(
-      [insructions_part, prompt],
-      generation_config=generation_config,
-      safety_settings=self.safety_settings,
-      stream=False,
+    generation_config = types.GenerateContentConfig(
+      temperature=1,
+      top_k=40,
+      top_p=0.95,
+      max_output_tokens=8192,
+      response_modalities = ["TEXT"],
+      safety_settings=self.safety_settings
     )
+
+    instructions_part = Part.from_text(text=text)
+    prompt_part = Part.from_text(text=f"""convert this text to structured YAML""")
+
+    contents = [
+      types.Content(
+        role="user",
+        parts=[instructions_part, prompt_part]
+      )
+    ]
+    response = self.client.models.generate_content(
+      model=self.model_name,
+      contents = contents,
+      config=generation_config
+      )
 
     logger.debug(response.text)
 
     return response.text
 
 
-  def initialize_ai(self,project_id, region, model_name=DEFAULT_MODEL_NAME, safety_setting=DEFAULT_SAFETY_SETTING) -> None:
+
+  def initialize_ai(self,project_id, region, model_name=DEFAULT_MODEL_NAME) -> None:
 
     self.model_name = model_name
-    self.safety_settings = [
-        SafetySetting(
-            category=SafetySetting.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-            threshold=safety_setting
-        ),
-        SafetySetting(
-            category=SafetySetting.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-            threshold=safety_setting
-        ),
-        SafetySetting(
-            category=SafetySetting.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-            threshold=safety_setting
-        ),
-        SafetySetting(
-            category=SafetySetting.HarmCategory.HARM_CATEGORY_HARASSMENT,
-            threshold=safety_setting
-        ),
-    ]
 
-    vertexai.init(project=project_id, location=region)
-    self.model = GenerativeModel(self.model_name)
+    self.safety_settings = [types.SafetySetting(
+          category="HARM_CATEGORY_HATE_SPEECH",
+          threshold="OFF"
+        ),types.SafetySetting(
+          category="HARM_CATEGORY_DANGEROUS_CONTENT",
+          threshold="OFF"
+        ),types.SafetySetting(
+          category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
+          threshold="OFF"
+        ),types.SafetySetting(
+          category="HARM_CATEGORY_HARASSMENT",
+          threshold="OFF"
+        )]
+    self.client = genai.Client(
+      vertexai=True,
+      project=project_id,
+      location=region
+    )
+
+    
+
+
